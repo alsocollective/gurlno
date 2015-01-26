02d34d5d71ab4441e9c146c6c269413d5f51ab0a
Bohdan's desktop <bohdan.anderson@gmail.com>
2015-01-25 22:50:45 -0500

    updates

--- application/galleries/views.py
+++ application/galleries/views.py
@@ -25,8 +25,9 @@ def currentshow(request,slug):
 	try:
 		now = offSetTimeBy(-5).date()
 		gallery = Gallery.objects.get(slug = slug)
-		show = Show.objects.all().filter(date_end__gte=now, gallery=gallery)[0]
-		out = {"gal":gallery,"show":show}
+		show = Show.objects.all().filter(gallery=gallery,date_end__gte=now)[0]
+		upcoming = Show.objects.all().filter(gallery=gallery,date_start__gt=now)
+		out = {"gal":gallery,"show":show,"upcoming":upcoming}
 	except Exception, e:
 		print e
 		out = {"erro":"no show found by that name"}
